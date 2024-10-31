3630c3134d7048e057275cfddb758bba9e089636
Syifaar <syifaarifah29@gmail.com>
2024-10-31 08:33:43 +0700

    - Memisahkan item makanan dan minuman - menambhakan item minuman

--- app/src/main/java/lat/pam/utsproject/ListFoodActivity.kt
+++ app/src/main/java/lat/pam/utsproject/ListFoodActivity.kt
@@ -10,9 +10,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class ListFoodActivity : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: FoodAdapter
+    private lateinit var foodrecyclerView: RecyclerView
+    private lateinit var drinkrecyclerView: RecyclerView
+    private lateinit var foodadapter: FoodAdapter
+    private lateinit var drinkadapter: FoodAdapter
     private lateinit var foodList: List<Food>
+    private lateinit var drinkList: List<Food>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,25 +24,43 @@ class ListFoodActivity : AppCompatActivity() {
         setContentView(R.layout.activity_list_food)
 
 
-        recyclerView = findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        foodrecyclerView = findViewById(R.id.foodrecyclerView)
+        drinkrecyclerView = findViewById(R.id.drinkrecyclerView)
+
+        foodrecyclerView.layoutManager = LinearLayoutManager(this)
+        drinkrecyclerView.layoutManager = LinearLayoutManager(this)
 
         // Menyiapkan data makanan
         foodList = listOf(
             Food("Batagor", "Batagor asli enak dari Bandung", R.drawable.batagor),
             Food("Black Salad", "Salad segar yang dibuat secara langsung", R.drawable.black_salad),
-            Food("Cappucino", "Kopi cappucino asli yang dibuat dari Kopi Arabica", R.drawable.cappuchino),
             Food("Cheesecake", "chessecake enak sedunia", R.drawable.cheesecake),
             Food("Cireng", "Cireng Endul", R.drawable.cireng),
             Food("donut","Donut manis penggugah rasa",R.drawable.donut),
         )
 
-        adapter = FoodAdapter(foodList) { selectedFood ->
+        drinkList = listOf(
+            Food("Cappuccino", "Kopi cappuccino asli yang dibuat dari Kopi Arabica", R.drawable.cappuchino),
+            Food("Cendol", "Cendol segar dengan citarasa endul", R.drawable.cendol),
+            Food("Es Buah", "Es Buah menyegarkan dahaga", R.drawable.esbuah),
+            Food("Strawberry Mocktail", "Buah Strawberry asli segar dipetik langsung dari perkebunan ciwidey", R.drawable.strawmocktail),
+            Food("Blue Ocean", "Minuman segar Best Seller", R.drawable.blueocean),
+        )
+
+        foodadapter = FoodAdapter(foodList) { selectedFood ->
             val  intent = Intent(this, OrderActivity::class.java)
             intent.putExtra("foodName", selectedFood.name)
             startActivity(intent)
         }
-        recyclerView.adapter = adapter
+        foodrecyclerView.adapter = foodadapter
+
+        // Menyiapkan adapter untuk minuman
+        drinkadapter = FoodAdapter(drinkList) { selectedFood ->
+            val intent = Intent(this, OrderActivity::class.java)
+            intent.putExtra("foodName", selectedFood.name)
+            startActivity(intent)
+        }
+        drinkrecyclerView.adapter = drinkadapter
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
